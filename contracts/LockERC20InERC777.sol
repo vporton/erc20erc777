d3339ec6cf4d46d20180f6e35931b0fb3ca3ed4a
Victor Porton <porton.victor@gmail.com>
2023-06-10 21:52:14 +0300

    added multiplier/divisor

--- contracts/LockERC20InERC777.sol
+++ contracts/LockERC20InERC777.sol
@@ -7,23 +7,32 @@ import { ERC777 } from './ERC777.sol';
 import { IERC20 } from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 
 contract LockERC20InERC777 is ERC777 {
-    IERC20 erc20;
+    IERC20 private erc20;
+    uint private multiplier;
+    uint private divisor;
 
-    constructor(IERC20 _erc20, string memory name_, string memory symbol_/*, address[] memory defaultOperators_*/)
+    constructor(
+        IERC20 _erc20,
+        uint _multiplier,
+        uint _divisor,
+        string memory name_,
+        string memory symbol_
+        /*, address[] memory defaultOperators_*/
+    )
         ERC777(name_, symbol_, new address[](0))
     {
         erc20 = _erc20;
     }
 
     function borrowERC20(uint256 _amount, address _from, address _to, bytes memory userData) public {
-        _mint(_to, _amount, userData, "");
+        _mint(_to, _amount * multiplier / divisor, userData, "");
         erc20.transferFrom(_from, address(this), _amount);
         // To protect from reentrancy vulnerability, don't change variables below.
         emit BorrowedERC20(msg.sender, _amount, _from, _to);
     }
 
     function burn(uint256 _amount, bytes memory _data) public override {
-        _burn(msg.sender, _amount, _data, "");
+        _burn(msg.sender, _amount * multiplier / divisor, _data, "");
         // To protect from reentrancy vulnerability, don't change variables below.
         erc20.transfer(msg.sender, _amount);
     }
